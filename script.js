31c8ade2a4c9d6359a1b89f1099e1b9fe6ab8343
Kousek <jan.kus@post.cz>
2023-03-29 22:32:26 +0200

    2

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 document.querySelector('#buttons button.refresh').style.display = 'none';
+document.querySelector('h4').style.display = 'none';
+var count = 0;
 
 function moveButton() {
     var button = document.querySelector('#buttons button.no');
     button.style.position = 'absolute';
     button.style.left = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth)) + 'px';
     button.style.top = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight)) + 'px';
+    count++;
 }
 
 function showRefresh() {
@@ -13,6 +16,11 @@ function showRefresh() {
     document.querySelector('#buttons button.no').style.display = 'none';
     document.querySelector('#buttons button.refresh').style.display = '';
     document.querySelector('h1').innerHTML = 'Yeah, you\'re right!';
+    document.querySelector('h4').style.display = '';
+
+    if(count > 0) {
+        document.querySelector('h4').innerHTML = 'You really clicked on "yes" ' + count + ' times before you knew it?';
+    }
 }
 
 function refreshPage() {
